e6b364031199fd5f7e475fdad419dc38e34f9940
SERVER7Z\abels <abelshimeles1998@gmail.com>
2024-01-05 15:17:52 +0300

    edited visualize.py to create a model that can classify barbell exercises and count repetitions

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,6 +4,7 @@ from glob import glob
 files = glob("../../data/raw/MetaMotion/*.csv")
 data_path = "../../data/raw/MetaMotion"
 
+
 def read_data_from_files(files, data_path):
     acc_df = pd.DataFrame()
     gyr_df = pd.DataFrame()
@@ -12,10 +13,10 @@ def read_data_from_files(files, data_path):
     gyr_set = 1
 
     for f in files:
-        participant = f.split("-")[0].replace(data_path,"").strip("\\")
+        participant = f.split("-")[0].replace(data_path, "").strip("\\")
         label = f.split("-")[1]
         category = f.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
-        
+
         df = pd.read_csv(f)
 
         df["participant"] = participant
@@ -31,7 +32,7 @@ def read_data_from_files(files, data_path):
             df["set"] = gyr_set
             gyr_set += 1
             gyr_df = pd.concat([gyr_df, df])
-        
+
     acc_df.index = pd.to_datetime(acc_df["epoch (ms)"], unit="ms")
     gyr_df.index = pd.to_datetime(gyr_df["epoch (ms)"], unit="ms")
 
@@ -48,7 +49,7 @@ def read_data_from_files(files, data_path):
 
 acc_df, gyr_df = read_data_from_files(files, data_path)
 
-data_merged = pd.concat([acc_df.iloc[:,:3], gyr_df], axis=1)
+data_merged = pd.concat([acc_df.iloc[:, :3], gyr_df], axis=1)
 
 data_merged.columns = [
     "acc_x",
@@ -57,30 +58,31 @@ data_merged.columns = [
     "gyr_x",
     "gyr_y",
     "gyr_z",
+    "participant",
     "label",
     "category",
-    "participant",
-    "set" 
+    "set",
 ]
 
 sampling = {
-        'acc_x': "mean", 
-        'acc_y': "mean", 
-        'acc_z': "mean", 
-        'gyr_x': "mean", 
-        'gyr_y': "mean", 
-        'gyr_z': "mean", 
-        'participant': "last", 
-        'label' : "last",
-        'category': "last", 
-        'set': "last"
+    "acc_x": "mean",
+    "acc_y": "mean",
+    "acc_z": "mean",
+    "gyr_x": "mean",
+    "gyr_y": "mean",
+    "gyr_z": "mean",
+    "label": "last",
+    "category": "last",
+    "participant": "last",
+    "set": "last",
 }
 
 days = [g for n, g in data_merged.groupby(pd.Grouper(freq="D"))]
-data_resampled = pd.concat([df.resample(rule="200ms").apply(sampling).dropna() for df in days])
+data_resampled = pd.concat(
+    [df.resample(rule="200ms").apply(sampling).dropna() for df in days]
+)
 
 data_resampled["set"] = data_resampled["set"].astype("int")
 data_resampled.info()
 
 data_resampled.to_pickle("../../data/interim/01_data_processed.pkl")
-
---
+++ src/visualization/visualize.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from IPython.display import display
+
+mpl.style.use("seaborn-v0_8-deep")
+mpl.rcParams["figure.figsize"] = [20, 5]
+mpl.rcParams["figure.dpi"] = 100
+
+df = pd.read_pickle("../../data/interim/01_data_processed.pkl")
+
+labels = df["label"].unique()
+participants = df["participant"].unique()
+
+for label in labels:
+    for participant in participants:
+        combined_plot_df = (
+            df.query(f"label == '{label}'")
+            .query(f"participant == '{participant}'")
+            .reset_index()
+        )
+
+        if len(combined_plot_df) > 0:
+            fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(20, 10))
+            combined_plot_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax[0])
+            combined_plot_df[["gyr_x", "gyr_y", "gyr_z"]].plot(ax=ax[1])
+
+            ax[0].legend(
+                loc="upper center",
+                bbox_to_anchor=(0.5, 1.15),
+                ncol=3,
+                fancybox=True,
+                shadow=True,
+            )
+            ax[1].legend(
+                loc="upper center",
+                bbox_to_anchor=(0.5, 1.15),
+                ncol=3,
+                fancybox=True,
+                shadow=True,
+            )
+            ax[1].set_xlabel("samples")
+            plt.savefig(f"../../reports/figures/{label.title()} ({participant}).png")
+            plt.show()
